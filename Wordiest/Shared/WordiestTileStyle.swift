520fb167d4b2a7205429404cf3f206339a4dd943
Jesse Vincent <jesse@fsck.com>
2025-12-14 21:51:16 -0800

    Nudge tile bonus labels from edges

--- Wordiest/Shared/WordiestTileStyle.swift
+++ Wordiest/Shared/WordiestTileStyle.swift
@@ -25,8 +25,8 @@ enum WordiestTileStyle {
     static let padding3dpRatio: CGFloat = 3.0 / 64.0
     static let padding6dpRatio: CGFloat = 6.0 / 64.0
     // Android baseline ratios: using these yields closer parity than padding-based placement.
-    static let bonusTopBaselineFromTopRatio: CGFloat = 11.0 / 80.0
-    static let bonusBottomBaselineFromBottomRatio: CGFloat = 2.0 / 80.0
+    static let bonusTopBaselineFromTopRatio: CGFloat = 11.5 / 80.0
+    static let bonusBottomBaselineFromBottomRatio: CGFloat = 2.5 / 80.0
     static let valueBaselineFromBottomRatio: CGFloat = 15.0 / 80.0
 
     static func height(forWidth width: CGFloat) -> CGFloat {
